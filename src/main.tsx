a0aa90638769cf9a547fe016196315263923f63e
edutalksorg <megamart.dvst@gmail.com>
2026-04-09 16:09:55 +0530

    Refactor React.StrictMode to use StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,9 @@
 //   </StrictMode>,
 // )
 
-import React from 'react'
+
+
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
@@ -30,8 +32,11 @@ function LoadingScreen() {
   )
 }
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <LoadingScreen />
-  </React.StrictMode>
+  </StrictMode>,
 )
+
+
+
