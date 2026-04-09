b1e8c2ad4fff117f1290c092d8b70c9e4c9bae19
edutalksorg <megamart.dvst@gmail.com>
2026-04-09 16:08:05 +0530

    Implement LoadingScreen component in main.tsx
    Replaced the main app rendering with a loading screen component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,37 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+// import { ThemeProvider } from './context/ThemeContext.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <ThemeProvider>
+//       <App />
+//     </ThemeProvider>
+//   </StrictMode>,
+// )
+
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { ThemeProvider } from './context/ThemeContext.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
+function LoadingScreen() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '24px'
+    }}>
+      Loading...
+    </div>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LoadingScreen />
+  </React.StrictMode>
 )
